180a633b06170fd267bcf6741839870bcae50d8e
idyweb <inyangidongesit22@gmail.com>
2023-09-09 18:17:00 +0100

    worked perfectly

--- stage1.py
+++ stage1.py
@@ -1,22 +1,37 @@
-from flask import Flask, request, jsonify 
+from flask import Flask, request
+from datetime import datetime
+import pytz 
 
 app = Flask(__name__)
 
+github_url_source = 'https://github.com/idyweb/HNGx'
 
-@app.route('/slack_details', methods=['GET'])
+
+@app.route('/api', methods=['GET'])
 def slack_details():
     slack_name = request.args.get('slack_name')
-    # current_day = request.args.get('current_day')
-    # current_time = request.args.get('current_time')
     track = request.args.get('track')
-    # github_url_file = request.args.get('file_url')
-    # github_url_source= request.args.get('source_url')
-    # status = request.args.get('status')
+    current_day = datetime.now(pytz.utc).astimezone(pytz.timezone('Africa/Lagos')).strftime('%A')
+    current_time = datetime.now(pytz.utc).astimezone(pytz.timezone('Africa/Lagos'))
+    allowed_time_window = 2  # +/- 2 minutes
+    current_time_str = current_time.strftime('%H:%M:%S')
+
+    time_window = current_time.replace(second=0, microsecond=0) - datetime.now(pytz.utc).astimezone(pytz.timezone('Africa/Lagos')).replace(second=0, microsecond=0)
+    time_window_in_minutes = abs(time_window.total_seconds()) / 60
+    current_datetime = datetime.now()
+    
+    github_url_file =  'https://github.com/idyweb/HNGx/blob/master/stage1.py'
+    github_url_source= 'https://github.com/idyweb/HNGx'
     
 
     result = {
         'slack_name': slack_name,
-        'track': track
+        'current_day': current_day,
+        'current_utc_time': current_time_str,
+        'track': track,
+        'github_file_url': github_url_file,
+        'github_repo_url': github_url_source,
+        'status_code': 200
 
     }
     return result
